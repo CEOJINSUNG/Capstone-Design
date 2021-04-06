46aeb147a8c56f35c1290970cc971ba1fff33c46
Lee ki chan <gc0216@naver.com>
2021-04-06 20:39:44 +0900

    backend initial commit

---
+++ src/main/java/com/capstone/fans/FansApplication.java
@@ -0,0 +1,14 @@
+package com.capstone.fans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+
+@SpringBootApplication
+public class FansApplication {
+	public static void main(String[] args){
+		SpringApplication.run(FansApplication.class, args);
+	}
+}
